659d049192856852d3bcdf1a74787a331b9f7544
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-02-10 11:12:12 +0100

    Modification de l'interface de SpectrumStream, description du travail à faire.

--- WhistlePro/src/stream/SpectrumStream.java
+++ WhistlePro/src/stream/SpectrumStream.java
@@ -2,16 +2,34 @@ package stream;
 
 import common.DataListenerInterface;
 import common.DataSourceInterface;
+import common.JobProviderInterface;
 import java.util.ArrayList;
 
 public class SpectrumStream 
-    extends DataSourceInterface<ArrayList<Double>> 
-    implements DataListenerInterface<ArrayList<Double>> 
+    extends 
+        // Output an arraylist of double (the spectrum)
+        DataSourceInterface<ArrayList<Double>> 
+    implements 
+        // Receive an arraylist of double as input (the signal itself, smoothed or not)
+        DataListenerInterface<ArrayList<Double>>,
+        // Define a possible parallel job 
+        JobProviderInterface
 {
 
     @Override
-    public void onPushData(ArrayList<Double> data) {
-       // TODO: create jobs to compute spectrum
+    public void onPushData(DataSourceInterface<ArrayList<Double>> source, ArrayList<Double> data) {
+       // TODO: Store the data to compute spectrum
+    }
+
+    @Override
+    public void doWork() {
+        // TODO: do the computation work and put the data (cf DataSourceInterface)
+    }
+
+    @Override
+    public boolean isWorkAvailable() {
+        // TODO: If enough data is available for work, return true
+        return false;
     }
 
 }
