9575a73860a9371b5b5dae07d7b334abe0085fad
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-02-19 10:19:59 +0100

    Modification classe MainApplication qui implémente main.

--- WhistlePro/src/main/MainApplication.java
+++ WhistlePro/src/main/MainApplication.java
@@ -1,18 +1,36 @@
+package fr.enst.pact34.main;
+
 import stream.*;
 
 class MainApplication {
 
-    public static int main(String[] args) {
+    public static void main(String[] args) {
+
+        //////////////////////////////
+        // STREAM INITIALIZATION
+        //////////////////////////////
         AcquisitionStream acquisitionStream = new AcquisitionStream();
         SpectrumStream spectrumStream = new SpectrumStream();
         MfccFeatureStream mfccFeatureStream = new MfccFeatureStream();
         ClassificationStream classificationStream = new ClassificationStream();
 
+        //////////////////////////////
+        // STREAM INPUT-OUTPUT CONFIGURATION
+        //////////////////////////////
+        
+        // ACQUISITION ---> SPECTRUM
         acquisitionStream.subscribe(spectrumStream);
+
+        // SPECTRUM ---> MFCC
         spectrumStream.subscribe(mfccFeatureStream);
+
+        // MFCC ---> CLASSIFICATION
         mfccFeatureStream.subscribe(classificationStream);
+
+        //////////////////////////////
+        // CLASSIFICATION FEATURES CONFIGURATION
+        //////////////////////////////
         classificationStream.addFeatureProvider(mfccFeatureStream);
 
-        return 0;
     }
 }
