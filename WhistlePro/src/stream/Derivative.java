7298dca3eed234a87c9368eb4ab3699520d656c2
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-02-26 03:46:02 +0100

    Ajout d'un flux de dérivation (non fini).

---
+++ WhistlePro/src/stream/Derivative.java
@@ -0,0 +1,61 @@
+package fr.enst.pact34.whistlepro.stream;
+
+import java.util.ArrayList;
+
+import common.DataListenerInterface;
+import common.DataSourceInterface;
+import common.JobProviderInterface;
+import common.ConvolutionInterface;
+import common.Convolution1D;
+
+class Derivative
+    extends DataSourceInterface<ArrayList<Double>>
+    implements DataListenerInterface<ArrayList<Double>>, 
+               JobProviderInterface
+{
+    private ConvolutionInterface convolution;
+    private int n;
+
+    private ArrayList<ArrayList<Double>> storedData = new ArrayList<>();
+    private double[] buffer;
+
+    public Derivative(int nbMean) {
+        this.n = nbMean;
+        double[] kernel = new double[nbMean*2];
+        
+        for(int i=0; i<nbMean*2; ++i) kernel[i] = i <nbMean ? -1 : 1;
+
+        convolution = new Convolution1D(2*nbMean, kernel);
+    }
+
+    public void onPushData(DataSourceInterface<ArrayList<Double>> source, ArrayList<ArrayList<Double>> data) {
+        this.storedData.addAll(data);
+        // TODO: store the data to compute the derivative incrementally
+    }
+
+    @Override
+    public void onCommit(DataSourceInterface<ArrayList<Double>> source) {
+        // TODO: unlock data processing
+    }
+
+    @Override
+    public void onTransaction(DataSourceInterface<ArrayList<Double>> source) {
+        // TODO: use previous array 
+        this.buffer = new double[this.storedData.size()];
+        for(int i=0; i < this.storedData.size(); ++i) this.buffer[i] = (double) this.storedData.get(0).get(i); //FIXME get(0)
+        this.storedData.clear();   
+       // TODO: lock data processing
+    }
+
+    @Override
+    public void doWork() {
+        // TODO: use previous array
+        // -n is for boundsof the array
+        double[] output = convolution.convoluate(this.buffer,0, buffer.length-this.n); 
+    }
+    
+    @Override
+    public boolean isWorkAvailable() {
+        return false;
+    }
+}
