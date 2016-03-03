2be3fd880bd1ac4f4c6df4988f538893fd8b8642
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-03-03 23:05:52 +0100

    Ajout DataSource.java.

---
+++ WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/common/DataSource.java
@@ -0,0 +1,61 @@
+package fr.enst.pact34.whistlepro.api.common;
+
+import java.util.HashSet;
+import java.util.ArrayList;
+import fr.enst.pact34.whistlepro.api.common.DataListenerInterface;
+
+//TODO: documentation
+public class DataSource<E> implements DataSourceInterface<E>{
+
+    private HashSet<DataListenerInterface<E>> listeners = new HashSet<>();
+
+    //////////////////////////////
+    /// @brief add a DataListenerInterface as output
+    /// @param listener the chained ouput
+    //////////////////////////////
+    public final void subscribe(DataListenerInterface<E> listener) {
+        if (this.listeners.contains(listener)) return;
+        this.listeners.add(listener); 
+    }
+
+    //////////////////////////////
+    /// @brief remove a DataListenerInterface as output
+    /// @param listener the listener to remove as chained output
+    //////////////////////////////
+    public final void unsubscribe(DataListenerInterface<E> listener) {
+        if (!this.listeners.contains(listener)) return;
+        this.listeners.remove(listener);
+    }
+    
+    //////////////////////////////
+    /// @brief push data to listeners
+    /// @param outputData the data to broadcast
+    //////////////////////////////
+    public final void push(ArrayList<E> outputData) {
+        for (DataListenerInterface<E> out : this.listeners)
+        {
+            out.onPushData(this, outputData);
+        }
+    }
+
+
+    //////////////////////////////
+    /// @brief allow following module to process data
+    //////////////////////////////
+    public final void commit() {
+        for (DataListenerInterface<E> out : this.listeners)
+        {
+            out.onCommit(this);
+        }
+    }
+
+    //////////////////////////////
+    /// @brief allow following module to process data
+    //////////////////////////////
+    public final void transaction() {
+        for (DataListenerInterface<E> out : this.listeners)
+        {
+            out.onTransaction(this);
+        }
+    }
+}
