cfed13b83da8d3a5a104398b85bdd86e1372951c
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-03-03 22:59:03 +0100

    Derivative implementation.

--- WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/stream/Derivative.java
+++ WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/stream/Derivative.java
@@ -1,4 +1,4 @@
-package fr.enst.pact34.whistlepro.stream;
+package fr.enst.pact34.whistlepro.api.stream;
 
 import java.util.ArrayList;
 
@@ -12,20 +12,22 @@ import fr.enst.pact34.whistlepro.api.common.Convolution1D;
 class Derivative
     //extends DataSource<ArrayList<Double>>
     implements
-        DataSourceInterface<ArrayList<Double>>,
-        DataListenerInterface<ArrayList<Double>>,
+        DataSourceInterface<Double>,
+        DataListenerInterface<Double>,
         JobProviderInterface
 {
-    private DataSource<ArrayList<Double>> datasource = new DataSource<>();
+    private DataSource<Double> datasource = new DataSource<>();
 
     private ConvolutionInterface convolution;
     private int n;
 
-    private ArrayList<ArrayList<Double>> storedData = new ArrayList<>();
+    private ArrayList<Double> storedData = new ArrayList<>();
+    private double[] lastbuffer;
     private double[] buffer;
 
     public Derivative(int nbMean) {
         this.n = nbMean;
+        this.lastbuffer = new double[this.n];
         double[] kernel = new double[nbMean*2];
         
         for(int i=0; i<nbMean*2; ++i) kernel[i] = i <nbMean ? -1 : 1;
@@ -33,32 +35,30 @@ class Derivative
         convolution = new Convolution1D(2*nbMean, kernel);
     }
 
-    public void onPushData(DataSource<ArrayList<Double>> source, ArrayList<ArrayList<Double>> data) {
+    public void onPushData(DataSource<Double> source, ArrayList<Double> data) {
         this.storedData.addAll(data);
         // TODO: store the data to compute the derivative incrementally
     }
 
     @Override
-    public void subscribe(DataListenerInterface<ArrayList<Double>> listener) {
+    public void subscribe(DataListenerInterface<Double> listener) {
         this.datasource.unsubscribe(listener);
     }
 
     @Override
-    public void unsubscribe(DataListenerInterface<ArrayList<Double>> listener) {
+    public void unsubscribe(DataListenerInterface<Double> listener) {
         this.datasource.unsubscribe(listener);
     }
 
     @Override
-    public void onCommit(DataSource<ArrayList<Double>> source) {
+    public void onCommit(DataSource<Double> source) {
         // TODO: unlock data processing
     }
 
     @Override
-    public void onTransaction(DataSource<ArrayList<Double>> source) {
+    public void onTransaction(DataSource<Double> source) {
         // TODO: use previous array 
-        this.buffer = new double[this.storedData.size()];
-        for(int i=0; i < this.storedData.size(); ++i) this.buffer[i] = (double) this.storedData.get(0).get(i); //FIXME get(0)
-        this.storedData.clear();   
+
        // TODO: lock data processing
     }
 
@@ -66,12 +66,30 @@ class Derivative
     public void doWork() {
         // TODO: use previous array
         // -n is for boundsof the array
-        double[] output = convolution.convoluate(this.buffer,0, buffer.length-this.n);
+        assert(this.storedData.size()>this.n);
 
+        boolean useLastBuffer = this.lastbuffer.length>this.n;
+        int start = useLastBuffer ? this.n : 0;
+        int size = useLastBuffer ? this.n + this.storedData.size() : this.storedData.size();
+        this.buffer = new double[size];
+
+        if (useLastBuffer) for(int i=0; i<n; ++i) this.buffer[i] = this.lastbuffer[i];
+        for(int i=start; i < size; ++i) {
+            this.buffer[i] = this.storedData.get(i); //FIXME get(0)
+
+        }
+        this.storedData.clear();
+
+        double[] output = convolution.convoluate(this.buffer,0, buffer.length-this.n);
+        ArrayList<Double> toPush = new ArrayList<>();
+        toPush.ensureCapacity(output.length);
+        for(int i=0; i<output.length; ++i) toPush.add(output[i]);
+
+        this.datasource.push(toPush);
     }
     
     @Override
     public boolean isWorkAvailable() {
-        return false;
+        return this.storedData.size() > this.n;
     }
 }
