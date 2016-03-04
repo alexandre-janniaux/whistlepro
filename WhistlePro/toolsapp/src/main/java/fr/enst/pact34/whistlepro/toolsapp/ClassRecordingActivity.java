81ea49bb6638361873a614d3ac10adbd6160e333
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-03-04 18:25:29 +0100

    Ajout d'une activité pour faire l'enregistrement des onomatopées.

---
+++ WhistlePro/toolsapp/src/main/java/fr/enst/pact34/whistlepro/toolsapp/ClassRecordingActivity.java
@@ -0,0 +1,13 @@
+package fr.enst.pact34.whistlepro.toolsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassRecordingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_recording);
+    }
+}
