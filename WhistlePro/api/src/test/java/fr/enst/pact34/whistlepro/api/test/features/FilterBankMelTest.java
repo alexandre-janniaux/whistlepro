2038022337ab99525b8461451fb20e98e82fb109
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-03-04 13:25:20 +0100

    Déplacement FilterBankMelTest.

--- WhistlePro/api/src/test/java/fr/enst/pact34/whistlepro/api/test/features/FilterBankMelTest.java
+++ WhistlePro/api/src/test/java/fr/enst/pact34/whistlepro/api/test/features/FilterBankMelTest.java
@@ -2,27 +2,65 @@ package fr.enst.pact34.whistlepro.api.test.features;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import fr.enst.pact34.whistlepro.api.common.FileOperator;
 import org.junit.Test;
 import fr.enst.pact34.whistlepro.api.features.FilterBankMel;
+
+import java.util.ArrayList;
 
 public class FilterBankMelTest {
 
     @Test
     public void frequencyToMel() {
-        // TODO: implement test
-        throw new UnsupportedOperationException("Le test de calcul du signal en échelle de mél doit être fait");
+        double freq = 100., mel = 150.48987948783699;
+        assertEquals(FilterBankMel.frequencyToMel(freq),mel,mel*0.001); // TODO: precalculate mel coeff and round it
     }
 
     @Test
     public void melToFrequency() {
-        double mel = 100., freq = 100.;
-        assertTrue(FilterBankMel.melToFrequency(100.)-freq <= Double.MIN_VALUE); // TODO: precalculate mel coeff and round it
+        double mel = 100., freq = 64.950770658278702;
+        assertEquals(FilterBankMel.melToFrequency(mel),freq,freq*0.001); // TODO: precalculate mel coeff and round it
     }
 
     @Test
     public void computeFilterFrequencies()
     {
-        throw new UnsupportedOperationException("Implémenter le test computeFilterFrequencies");
+        double freqMin = 0, freqMax = 3500;
+        int nbMelFiler = 24;
+        int nbPtsEchelle = 350+1;
+        double[] echelleFreq = new double[nbPtsEchelle];
+        for(int i = 0; i < echelleFreq.length; i ++)
+        {
+            echelleFreq[i] = ((double)i/(echelleFreq.length-1))*(freqMax-freqMin);
+        }
+        assertEquals(freqMin,echelleFreq[0],Double.MIN_VALUE);
+        assertEquals(freqMax,echelleFreq[echelleFreq.length-1],Double.MIN_VALUE);
+
+        double[][] filters = FilterBankMel.computeFilterBank(freqMin,freqMax,nbMelFiler,echelleFreq);
+
+        ArrayList<String> lines = FileOperator.getLinesFromFile("../testData/features/genFilters.valid");
+
+        assertEquals(lines.size(),nbMelFiler);
+
+        for(int i = 0; i < filters.length; i++)
+        {
+            String[] line = lines.get(i).split(";");
+
+            assertEquals(nbPtsEchelle,line.length);
+
+            for(int j = 0; j < filters[i].length; j++)
+            {
+                double tmp = Double.parseDouble(line[j]);
+                if(tmp != 0)
+                    assertEquals(tmp,filters[i][j], Math.abs(tmp*0.001));
+                else
+                    assertEquals(tmp,filters[i][j],1e-14);
+            }
+        }
+
+
+
     }
 
 
