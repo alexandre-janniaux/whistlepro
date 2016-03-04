936bdb8aa9d50bc04bc827a898a96d6ddec29288
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-03-04 11:27:46 +0100

    Finalisation de multiplexerStream.

---
+++ WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/common/DataStreamInterface.java
@@ -0,0 +1,7 @@
+package fr.enst.pact34.whistlepro.api.common;
+
+/**
+ * Created by alexandre on 04/03/16.
+ */
+public interface DataStreamInterface<E,F> extends DataSourceInterface<F>, DataListenerInterface<E> {
+}
--- WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/stream/MultiplexerStream.java
+++ WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/stream/MultiplexerStream.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import fr.enst.pact34.whistlepro.api.common.DataListenerInterface;
 import fr.enst.pact34.whistlepro.api.common.DataSource;
 import fr.enst.pact34.whistlepro.api.common.DataSourceInterface;
+import fr.enst.pact34.whistlepro.api.common.DataStreamInterface;
 import fr.enst.pact34.whistlepro.api.common.FactoryInterface;
 import fr.enst.pact34.whistlepro.api.common.JobProviderInterface;
 
@@ -18,9 +19,11 @@ public class MultiplexerStream<E,F>
     private int n;
     private DataSource<ArrayList<F>> datasource = new DataSource<>();
 
-    private ArrayList<DataListenerInterface<E>> sources = new ArrayList<>();
+    private ArrayList<DataStreamInterface<E,F>> sources = new ArrayList<>();
     private ArrayList<FakeLoopStream> loops = new ArrayList<>();
     private ArrayList<ArrayList<F>> outputs = new ArrayList<>();
+
+    private int computed = 0;
 
     @Override
     public void subscribe(DataListenerInterface<ArrayList<F>> listener) { this.datasource.subscribe(listener); }
@@ -40,20 +43,45 @@ public class MultiplexerStream<E,F>
 
         @Override
         public void onPushData(DataSource<F> source, ArrayList<F> inputData) {
-            this.root.outputs.get(this.index) = inputData;
+            this.root.outputs.set(this.index,inputData);
+            this.root.computed += 1;
+            if (this.root.computed == this.root.n) {
+                ArrayList<ArrayList<F>> outputs = new ArrayList<>();
+                for (int i=0; i < inputData.size(); ++i) {
+                    ArrayList<F> array = new ArrayList<>();
+                    outputs.set(i, array);
+                    for (int j=0; j < this.root.n; ++j) {
+                        array.add(this.root.outputs.get(j).get(i));
+                    }
+                }
+                this.root.datasource.push(outputs);
+            }
+        }
+
+        @Override
+        public void onCommit(DataSource<F> source) {
+        }
+
+        @Override
+        public void onTransaction(DataSource<F> source) {
         }
 
     }
 
 
-    public MultiplexerStream(int n, FactoryInterface<DataListenerInterface<E>> source) {
+    public MultiplexerStream(int n, FactoryInterface<DataStreamInterface<E,F>> source) {
         this.n = n;
         this.sources.ensureCapacity(n);
+        this.loops.ensureCapacity(n);
         this.outputs.ensureCapacity(n);
 
         for (int i=0; i<n; ++i) {
-            this.sources.add(source.create());
-            //this.listeners.add(listener.create());
+            DataStreamInterface<E,F> input = source.create();
+            FakeLoopStream loop = new FakeLoopStream(i, this);
+            this.sources.add(input);
+            this.loops.add(loop);
+            loop.subscribe(input);
+            input.subscribe(loop);
         }
     }
 
@@ -63,7 +91,7 @@ public class MultiplexerStream<E,F>
 
         for(int i=0; i<n; ++i) {
             ArrayList<E> data = inputData.get(i);
-            this.sources.get(i).onPushData(null, data); // TODO: put a real datasource
+            this.loops.get(i).push(data);
         }
     }
 
