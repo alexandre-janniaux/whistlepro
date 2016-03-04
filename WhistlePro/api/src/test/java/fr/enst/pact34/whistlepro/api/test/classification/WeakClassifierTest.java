82464a20d37c37b0447e2a03f35fe5603830e3bb
Mohamed <noname.p@outlook.com>
2016-03-04 21:55:49 +0100

    Deplacement de fichier

--- WhistlePro/api/src/test/java/fr/enst/pact34/whistlepro/api/test/classification/WeakClassifierTest.java
+++ WhistlePro/api/src/test/java/fr/enst/pact34/whistlepro/api/test/classification/WeakClassifierTest.java
@@ -1,4 +1,4 @@
-package classification;
+package fr.enst.pact34.whistlepro.api.test.classification;
 
 import fr.enst.pact34.whistlepro.api.classification.FeaturedObjectInterface;
 import fr.enst.pact34.whistlepro.api.classification.WeakClassifier;
