0898f8bd544130c08065f703375e2c01552f0d81
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-03-06 08:27:59 +0100

    Ajout d'une classe pour envelopper un signal.

---
+++ WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/common/DoubleSignal.java
@@ -0,0 +1,28 @@
+package fr.enst.pact34.whistlepro.api.common;
+
+
+public class DoubleSignal {
+
+    private final double[] signal;
+    private final int nbSamples;
+    private final double sampleFrequency;
+
+    public DoubleSignal(double[] signal, int nbSamples, double sampleFrequency) {
+        this.signal = signal;
+        this.nbSamples = nbSamples;
+        this.sampleFrequency = sampleFrequency;
+    }
+
+    public double[] getSignal() {
+        return signal;
+    }
+
+    public int getNbSamples() {
+        return nbSamples;
+    }
+
+    public double getSampleFrequency() {
+        return sampleFrequency;
+    }
+
+}
