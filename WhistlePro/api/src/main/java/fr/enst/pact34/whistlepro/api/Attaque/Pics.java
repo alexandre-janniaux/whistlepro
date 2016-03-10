725e1a388714d028056196059898fe393bed597c
David <dj.jiawei@gmail.com>
2016-03-10 14:03:59 +0100

    Ajout d'une nouvelle methode dans la classe Pics pour ameliorer le nombre de pics obtenus

--- WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/Attaque/Pics.java
+++ WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/Attaque/Pics.java
@@ -1,6 +1,7 @@
 package fr.enst.pact34.whistlepro.api.Attaque;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by dell on 10/03/2016.
@@ -11,8 +12,8 @@ public class Pics {
         ArrayList<Double> pics = new ArrayList<Double>();
         ArrayList<Integer> picIndice = new ArrayList<Integer>();
         int len = x.length;
-        for (int i=1; i<len-1; i++) {
-            if (x[i-1]<x[i] && x[i+1]<x[i]) {
+        for (int i = 1; i < len - 1; i++) {
+            if (x[i - 1] < x[i] && x[i + 1] < x[i]) {
                 pics.add(x[i]);
                 picIndice.add(i);
             }
@@ -20,11 +21,44 @@ public class Pics {
 
         int length = pics.size();
         double[] picDouble = new double[length];
-        for (int i=0; i<length; i++) {
+        for (int i = 0; i < length; i++) {
+            picDouble[i] = (double) pics.get(i);
+        }
+
+        return picDouble;
+    }
+
+    public double[] PicsReduced(double[] x) {
+
+        ArrayList<Double> pics = new ArrayList<Double>();
+        ArrayList<Integer> picIndice = new ArrayList<Integer>();
+        int len = x.length;
+        int compteur = 0;
+
+        for (int j = 0; j < len; j++) {
+            ArrayList<Double> sousPics = new ArrayList<Double>();
+            if (compteur == 5) {
+                compteur = 0;
+                sousPics = null;
+                sousPics.add(x[j]);
+                for (int k = j - 4; k < j; k++) {
+                    if (x[k] == Collections.max(sousPics)) {
+                        pics.add(x[k]);
+                        picIndice.add(k);
+                    }
+                }
+            }
+            compteur++;
+        }
+
+        int length = pics.size();
+        double[] picDouble = new double[length];
+        for (int i = 0; i < length; i++) {
             picDouble[i] = (double) pics.get(i);
         }
 
         return picDouble;
 
     }
+
 }
