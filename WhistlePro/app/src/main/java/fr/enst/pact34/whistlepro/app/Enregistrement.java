c57bf97cb292d8512c956ed9af549b4abd5a050f
Salim BENKIRANE <salim.benkirane@telecom-paristech.fr>
2016-03-10 17:28:03 +0100

    Creation d'enregistrement + nommer les pistes

---
+++ WhistlePro/app/src/main/java/fr/enst/pact34/whistlepro/app/Enregistrement.java
@@ -0,0 +1,13 @@
+package fr.enst.pact34.whistlepro.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Enregistrement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enregistrement);
+    }
+}
