ba82cff7300f048bd667d307bdea02ad8b726f7c
David <dj.jiawei@gmail.com>
2016-03-10 14:07:44 +0100

    Ajout de commentaires pour les methodes de detection de pics

--- WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/Attaque/Pics.java
+++ WhistlePro/api/src/main/java/fr/enst/pact34/whistlepro/api/Attaque/Pics.java
@@ -8,18 +8,20 @@ import java.util.Collections;
  */
 public class Pics {
 
+    //Affiche une liste de pics avec la méthode naive qui consiste a prendre un point
+    // tant qu'il est superieur au point precedent ET au suivant
     public double[] DetectionPics(double[] x) {
-        ArrayList<Double> pics = new ArrayList<Double>();
+        ArrayList<Double> pics = new ArrayList<Double>();   //on prend des ArrayList car on ne sait pas cb de pics on aura
         ArrayList<Integer> picIndice = new ArrayList<Integer>();
         int len = x.length;
         for (int i = 1; i < len - 1; i++) {
-            if (x[i - 1] < x[i] && x[i + 1] < x[i]) {
+            if (x[i-1] < x[i] && x[i+1] < x[i]) {
                 pics.add(x[i]);
                 picIndice.add(i);
             }
         }
 
-        int length = pics.size();
+        int length = pics.size();   //on transforme l'ArrayList en liste de double
         double[] picDouble = new double[length];
         for (int i = 0; i < length; i++) {
             picDouble[i] = (double) pics.get(i);
@@ -28,6 +30,8 @@ public class Pics {
         return picDouble;
     }
 
+    //Il peut y avoir trop de pics à cause d'oscillations.
+    //Cet algorithme prend le point correspondant à la valeur maximale tous les 5 points.
     public double[] PicsReduced(double[] x) {
 
         ArrayList<Double> pics = new ArrayList<Double>();
@@ -51,7 +55,7 @@ public class Pics {
             compteur++;
         }
 
-        int length = pics.size();
+        int length = pics.size();  //on transforme l'ArrayList en liste de double
         double[] picDouble = new double[length];
         for (int i = 0; i < length; i++) {
             picDouble[i] = (double) pics.get(i);
