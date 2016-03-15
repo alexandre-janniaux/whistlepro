48583608684654bbb4fee700d700b55d6f192653
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2016-03-15 11:57:18 +0100

    [api] Suppression des logs pour AcquisitionStream.

--- WhistlePro/api/src/test/java/fr/enst/pact34/whistlepro/api/test/stream/AcquisitionStreamTest.java
+++ WhistlePro/api/src/test/java/fr/enst/pact34/whistlepro/api/test/stream/AcquisitionStreamTest.java
@@ -15,6 +15,8 @@ import static org.junit.Assert.assertTrue;
 
 public class AcquisitionStreamTest {
 
+
+    // IMPORTANT : le micro ne doit pas êeêêtre muté !!!!
     @Test
     public void isRecording() {
         final AcquisitionStream stream = new AcquisitionStream();
