aab0b167a20ad49e48549614413640b46b1ca720
Mohamed <noname.p@outlook.com>
2016-04-17 23:31:54 +0200

    Remplacement de retour vide (null) par des retours de valeur

---
+++ WhistlePro/api2/src/main/java/fr/enst/pact34/whistlepro/api2/DataTypes/Frequency.java
@@ -0,0 +1,46 @@
+package  fr.enst.pact34.whistlepro.api2.dataTypes;
+
+import  fr.enst.pact34.whistlepro.api2.stream.StreamDataInterface;
+
+/**
+ * Created by mms on 15/03/16.
+ */
+public class Frequency implements StreamDataInterface<Frequency> {
+
+    @Override
+    public void copyTo(Frequency o) {
+
+        o.id = this.id;
+        o.valid=this.valid;
+    }
+
+    @Override
+    public Frequency getNew() {
+        //TODO
+        return new Frequency();
+    }
+
+    int id = -1;
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    boolean valid = true;
+
+    @Override
+    public boolean isValid() {
+        return valid;
+    }
+
+    @Override
+    public void setValid(boolean v) {
+        valid =v;
+    }
+}
