d99cc86754166f06843685d9c4191838393e86c4
Mohamed <noname.p@outlook.com>
2016-04-26 17:55:14 +0200

    Ajout des attributs principaux

--- WhistlePro/api2/src/main/java/fr/enst/pact34/whistlepro/api2/main/PistePercu.java
+++ WhistlePro/api2/src/main/java/fr/enst/pact34/whistlepro/api2/main/PistePercu.java
@@ -1,9 +1,16 @@
 package fr.enst.pact34.whistlepro.api2.main;
 
+import java.util.List;
+
+import fr.enst.pact34.whistlepro.api2.dataTypes.AttackTimes;
+import fr.enst.pact34.whistlepro.api2.dataTypes.ClassifResults;
+
 /**
  * Created by mms on 25/04/16.
  */
 public class PistePercu extends Piste {
+    List<ClassifResults> onomatopees;
+    List<AttackTimes> attackTimes;
     @Override
     public TypePiste getTypePiste() {
         return TypePiste.Percussions;
