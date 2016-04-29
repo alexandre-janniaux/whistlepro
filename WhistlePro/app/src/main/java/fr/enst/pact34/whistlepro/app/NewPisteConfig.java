2d3619f2a6b8f31185423ad5866dd033e937bf4a
Mohamed <noname.p@outlook.com>
2016-04-29 11:13:40 +0200

    Ajout d'une activité pour le parametrage de l'enregistrement

---
+++ WhistlePro/app/src/main/java/fr/enst/pact34/whistlepro/app/NewPisteConfig.java
@@ -0,0 +1,15 @@
+package fr.enst.pact34.whistlepro.app;
+
+import android.os.Bundle;
+
+/**
+ * Created by mms on 29/04/16.
+ */
+public class NewPisteConfig extends WhistleProActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_piste_config);
+    }
+}
