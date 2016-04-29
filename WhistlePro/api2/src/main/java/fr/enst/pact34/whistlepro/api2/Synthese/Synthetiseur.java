565eeb26a53e730ea3baff27fccb59df8ac83357
Mohamed <noname.p@outlook.com>
2016-04-29 07:39:54 +0200

    Creation de la classe gerant la synthese, ajout de la partie percussion et test associé

---
+++ WhistlePro/api2/src/main/java/fr/enst/pact34/whistlepro/api2/Synthese/Synthetiseur.java
@@ -0,0 +1,82 @@
+package fr.enst.pact34.whistlepro.api2.Synthese;
+
+import java.util.List;
+
+import fr.enst.pact34.whistlepro.api2.dataTypes.Signal;
+import fr.enst.pact34.whistlepro.api2.main.Piste;
+import fr.enst.pact34.whistlepro.api2.main.PisteMelodie;
+import fr.enst.pact34.whistlepro.api2.main.PistePercu;
+
+/**
+ * Created by mms on 28/04/16.
+ */
+public class Synthetiseur {
+
+    private PercuGenerator percuGen = null;
+    private InstruGenerator instruGen = null;
+
+    public Synthetiseur(PercuGenerator percuGen, InstruGenerator instruGen)
+    {
+        this.percuGen = percuGen;
+        this.instruGen = instruGen;
+    }
+
+    public Signal synthetise (Piste piste)
+    {
+
+        switch (piste.getTypePiste())
+        {
+            case Melodie:
+                return synthetiseMelodie((PisteMelodie) piste);
+
+            case Percussions:
+                return synthetisePercu((PistePercu) piste);
+
+        }
+
+        return  null;
+    }
+
+    private Signal synthetisePercu(PistePercu pistePercu)
+    {
+        List<Percu> percussions = pistePercu.getPercuList();
+
+        double totalTime = pistePercu.getTotalTime();
+        double Fs = percuGen.getSamplingFreq();
+        Signal sound = new Signal();
+        sound.setLength((int) (Fs * totalTime));
+
+        for (int i = 0; i < percussions.size(); i++) {
+            Percu p = percussions.get(i);
+            Percu.Type type = p.getType();
+            double time = p.getEndTime() - p.getStartTime();
+            Signal sigGen = percuGen.generate(type,time);
+
+            sound.fromSignal(sigGen,0,(int)(Fs*p.getStartTime()),sigGen.length());
+        }
+
+        sound.setSamplingFrequency(Fs);
+        return sound;
+    }
+
+    private Signal synthetiseMelodie(PisteMelodie pisteMelodie)
+    {
+        List<Instru> percussions = pisteMelodie.getInstruList();
+
+        double totalTime = pisteMelodie.getTotalTime();
+        double Fs = instruGen.getSamplingFreq();
+        Signal sound = new Signal();
+        sound.setLength((int) (Fs * totalTime));
+
+        for (int i = 0; i < percussions.size(); i++) {
+            Instru p = percussions.get(i);
+            Instru.Type type = p.getType();
+            double time = p.getEndTime() - p.getStartTime();
+            Signal sigGen = instruGen.generate(type,time);
+
+            sound.fromSignal(sigGen,0,(int)(Fs*p.getStartTime()),sigGen.length());
+        }
+
+        return sound;
+    }
+}
---
+++ WhistlePro/api2/src/test/java/fr/enst/pact34/whistlepro/api2/test/synthese/SynthetiseurTest.java
@@ -0,0 +1,72 @@
+package fr.enst.pact34.whistlepro.api2.test.synthese;
+
+import org.junit.Test;
+
+import fr.enst.pact34.whistlepro.api2.Synthese.Percu;
+import fr.enst.pact34.whistlepro.api2.Synthese.PercuGenerator;
+import fr.enst.pact34.whistlepro.api2.Synthese.Synthetiseur;
+import fr.enst.pact34.whistlepro.api2.dataTypes.Signal;
+import fr.enst.pact34.whistlepro.api2.main.PistePercu;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by mms on 29/04/16.
+ */
+public class SynthetiseurTest {
+
+    @Test
+    public void testSynthtiseurPercu()
+    {
+        Signal inputData = new Signal();
+        double Fs = 16000;
+        inputData.setSamplingFrequency(Fs);
+        inputData.setLength((int) (0.020 * Fs));
+
+        double t_step = 1 / Fs;
+        for (int i = 0; i < inputData.length(); i++) {
+            inputData.setValue(i,  Math.sin(2.0 * Math.PI * 2000 * i * t_step));
+        }
+
+        PercuGenerator generator = new PercuGenerator(Fs);
+        generator.addPercu(Percu.Type.Kick, inputData);
+
+        Synthetiseur synthe = new Synthetiseur(generator,null);
+
+        PistePercu pistePercu = new PistePercu();
+        pistePercu.setTotalTime(0.1);
+
+        Percu p = new Percu();
+        p.setType(Percu.Type.Kick);
+        p.setStartTime(0.010);
+        p.setEndTime(0.080);
+
+        pistePercu.addPercu(p);
+
+        Signal sound = synthe.synthetise(pistePercu);
+
+        assertEquals(0.1, sound.length() / sound.getSamplingFrequency(), Double.MIN_VALUE);
+
+        double sum = 0;
+        for (int i = 0; i < Fs * 0.01 && i < sound.length(); i++) {
+            sum+= Math.abs(sound.getValue(i));
+        }
+
+        assertEquals(0, sum, Double.MIN_VALUE);
+
+        sum = 0;
+        for (int i = (int) (Fs * 0.01); i < Fs * 0.08 && i < sound.length(); i++) {
+            sum+= Math.abs(sound.getValue(i));
+        }
+
+        assertTrue(sum > 1);
+
+        sum = 0;
+        for (int i = (int) (Fs * 0.08); i < sound.length(); i++) {
+            sum+= Math.abs(sound.getValue(i));
+        }
+
+        assertEquals(0, sum, Double.MIN_VALUE);
+    }
+}
