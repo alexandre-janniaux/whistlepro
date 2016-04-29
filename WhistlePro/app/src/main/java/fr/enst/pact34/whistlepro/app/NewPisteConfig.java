ac66751e02fe6c2567b00c971f646a49a4ba6c1f
Mohamed <noname.p@outlook.com>
2016-04-29 11:25:20 +0200

    Activité de configuration de l'enregistrement pour une nouvelle piste complétée

--- WhistlePro/app/src/main/java/fr/enst/pact34/whistlepro/app/NewPisteConfig.java
+++ WhistlePro/app/src/main/java/fr/enst/pact34/whistlepro/app/NewPisteConfig.java
@@ -1,6 +1,14 @@
 package fr.enst.pact34.whistlepro.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import fr.enst.pact34.whistlepro.api2.main.ProcessorInterface;
+import fr.enst.pact34.whistlepro.api2.main.TypePiste;
 
 /**
  * Created by mms on 29/04/16.
@@ -11,5 +19,32 @@ public class NewPisteConfig extends WhistleProActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_piste_config);
+
+        final ProcessorInterface processor = (ProcessorInterface) getSharedData(SD_PROCESSING_MACINE);
+        if(processor == null)
+        {
+            throw new RuntimeException(SD_PROCESSING_MACINE + " should exist in shared data.");
+        }
+
+        ((Button) findViewById(R.id.NewPisteConfig_button_next)).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if( ((RadioButton)findViewById(R.id.NewPisteConfig_Percussion)).isChecked() ) {
+                            processor.init(TypePiste.Percussions);
+                        }
+                        else if( ((RadioButton)findViewById(R.id.NewPisteConfig_Melodie)).isChecked() )
+                        {
+                            processor.init(TypePiste.Melodie);
+                        }
+                        else
+                        {
+                            return;
+                        }
+
+                        startActivity(new Intent(NewPisteConfig.this, NewPisteRecord.class));
+                    }
+                }
+        );
     }
 }
