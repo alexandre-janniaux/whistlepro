3716756873318f32ef255c442944b9865082a2ab
Mohamed <noname.p@outlook.com>
2016-05-01 17:39:33 +0200

    Passage des delta mfcc sur une valeur

--- WhistlePro/api2/src/main/java/fr/enst/pact34/whistlepro/api2/features/MfccProcess.java
+++ WhistlePro/api2/src/main/java/fr/enst/pact34/whistlepro/api2/features/MfccProcess.java
@@ -11,29 +11,23 @@ import java.util.Arrays;
 
 public class MfccProcess implements FeatureProviderInterface, StreamProcessInterface<Spectrum,Signal>
 {
-    private final int NB_VAL_DELTA = 5; // nombre de valeur avant ou apres (donc total = 2*NB_VAL_DELTA)
+
     private final int NB_VAL_MFCC = 13;
     private final int COUNT_MFCC_TOTAL = NB_VAL_MFCC*3;
     private final int nbMelFilter = 24;
     private double minFrequency = 0, maxFrequency = 3500;
 
-    private double[][] lastMfcc = new double[2*NB_VAL_DELTA+1][NB_VAL_MFCC];
-    private double[] DMfccSum = new double[NB_VAL_MFCC];
-    private double[][] lastDMfcc = new double[2*NB_VAL_DELTA+1][NB_VAL_MFCC];
-    private double[] DDMfccSum = new double[NB_VAL_MFCC];
+    private double[] lastMfcc = new double[NB_VAL_MFCC];
+    private double[] lastDMfcc = new double[NB_VAL_MFCC];
 
-    private int dmfcc_index = 0;
+
 
     public MfccProcess() {
         for (int i = 0; i < lastMfcc.length; i++) {
-            for (int j = 0; j < lastMfcc[i].length; j++) {
-                lastMfcc[i][j] = 0;
-            }
+                lastMfcc[i]= 0;
         }
         for (int i = 0; i < lastDMfcc.length; i++) {
-            for (int j = 0; j < lastDMfcc[i].length; j++) {
-                lastDMfcc[i][j] = 0;
-            }
+                lastDMfcc[i]= 0;
         }
     }
 
@@ -119,65 +113,21 @@ public class MfccProcess implements FeatureProviderInterface, StreamProcessInter
 
         double mfcc[] = transformers.dct(filtered, 13);
 
-        /* Pb  ArrayIndexOutOfBoundsException
-        System.arraycopy(mfcc, 0, lastMfcc[mfcc_index], 0, 13);
-        for (int i=0; i<lastMfcc.length; ++i) {
-            for(int j=0; j<13; ++j) {
-                dmfcc[j] += (i < lastMfcc.length / 2 ? (-1) : 1) * lastMfcc[(i + mfcc_index) % lastMfcc.length][j];
-            }
-        }
-        System.arraycopy(dmfcc, 0, lastDMfcc[dmfcc_index], 0, 13);
-
-        for (int i=0; i<lastDMfcc.length; ++i) {
-            for(int j=0; j<13; ++j) {
-                ddmfcc[j] += (i < lastDMfcc.length / 2 ? (-1) : 1) * lastDMfcc[(i + dmfcc_index) % lastDMfcc.length][j];
-            }
-        }
-        */
-
-
-
-        double iRemove[] = lastMfcc[(dmfcc_index+NB_VAL_DELTA+1)%(2*NB_VAL_DELTA+1)];
-        // => dmfcc_index-NB_VAL_DELTA <=> dmfcc_index+NB_VAL_DELTA+1
-        double iCenter[] = lastMfcc[(dmfcc_index)];
-        double iNextCenter[] = lastMfcc[(dmfcc_index+1)%(2*NB_VAL_DELTA+1)];
-        double iAdd[] = mfcc;
-
-
         for (int i = 0; i < NB_VAL_MFCC; i++) {
-            DMfccSum[i] += +iRemove[i] - iCenter[i] + iNextCenter[i]+ iAdd[i]; //passage de center des + au -
-            newDmfcc[i] = DMfccSum[i]/(2*NB_VAL_DELTA);
+            newDmfcc[i] = mfcc[i] - lastMfcc[i];
         }
-
-        iRemove = lastDMfcc[(dmfcc_index+NB_VAL_DELTA+1)%(2*NB_VAL_DELTA+1)];
-        // => dmfcc_index-NB_VAL_DELTA <=> dmfcc_index+NB_VAL_DELTA+1
-        iCenter = lastDMfcc[(dmfcc_index)];
-        iNextCenter = lastDMfcc[(dmfcc_index+1)%(2*NB_VAL_DELTA+1)];
-        iAdd = newDmfcc;
-
-
         for (int i = 0; i < NB_VAL_MFCC; i++) {
-            DDMfccSum[i] += +iRemove[i] - iCenter[i] + iNextCenter[i] +iAdd[i]; //passage de center des + au -
-            newDDmfcc[i] = DMfccSum[i]/(2*NB_VAL_DELTA);
+            newDDmfcc[i] = newDmfcc[i] - lastDMfcc[i];
         }
 
-        dmfcc_index=(dmfcc_index+1)%(2*NB_VAL_DELTA+1);
-        int newValIndex = (dmfcc_index+NB_VAL_DELTA)%(2*NB_VAL_DELTA+1);
-        System.arraycopy(mfcc, 0, lastMfcc[newValIndex],0,NB_VAL_MFCC);
-        System.arraycopy(newDmfcc, 0, lastDMfcc[newValIndex],0,NB_VAL_MFCC);
-
-        dmfcc_index = (dmfcc_index+1)%(2*NB_VAL_DELTA+1);
-
-        //System.arraycopy(mfcc, 0, lastMfcc[dmfcc_index], 0, 13);
-
-        //System.arraycopy(lastMfcc[mfcc_index], 0, lastMfcc[dmfcc_index], 0, 13);
-        //dmfcc_index = (dmfcc_index+1)%(2*NB_VAL_DELTA+1);
+        System.arraycopy(mfcc,0,lastMfcc,0,NB_VAL_MFCC);
+        System.arraycopy(newDmfcc,0,lastDMfcc,0,NB_VAL_MFCC);
 
 
-        sigOut.setLength(39);
-        sigOut.fromArray(mfcc, 0, 13);
-        sigOut.fromArray(newDmfcc, 13, 13);
-        sigOut.fromArray(newDDmfcc, 26, 13);
+        sigOut.setLength(COUNT_MFCC_TOTAL);
+        sigOut.fromArray(mfcc, 0, NB_VAL_MFCC);
+        sigOut.fromArray(newDmfcc, 1*NB_VAL_MFCC, NB_VAL_MFCC);
+        sigOut.fromArray(newDDmfcc, 2*NB_VAL_MFCC, NB_VAL_MFCC);
 
 
         sigOut.setSamplingFrequency(sigIn.getFs());
