bfeec4e3c56287e411b97c3d1d89a3a9731d5b8f
Mohamed <noname.p@outlook.com>
2016-05-01 17:02:50 +0200

    Completion de l'activité d'affichage d'un morceau

--- WhistlePro/app/src/main/java/fr/enst/pact34/whistlepro/app/OpenMorceau.java
+++ WhistlePro/app/src/main/java/fr/enst/pact34/whistlepro/app/OpenMorceau.java
@@ -1,9 +1,18 @@
 package fr.enst.pact34.whistlepro.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.List;
+
 import fr.enst.pact34.whistlepro.api2.main.Morceau;
+import fr.enst.pact34.whistlepro.api2.main.Piste;
 
 /**
  * Created by mms on 01/05/16.
@@ -15,13 +24,75 @@ public class OpenMorceau extends WhistleProActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_morceau);
 
-        Morceau m = (Morceau) getSharedData(SD_MORCEAU_ACTUEL);
-        if(m == null)
+        morceau = (Morceau) getSharedData(SD_MORCEAU_ACTUEL);
+        if(morceau == null)
         {
             throw new RuntimeException("Aucun morceau ouvert !");
         }
 
-        ((TextView) findViewById(R.id.OpenMorceau_nom_morceau)).setText(m.getTitle());
+        ((TextView) findViewById(R.id.OpenMorceau_nom_morceau)).setText("Titre :" + morceau.getTitle());
+
+        ListView listPisteElements = (ListView) findViewById(R.id.OpenMorceau_listView_piste);
+
+        adapter = new ArrayAdapter<>(this,R.layout.new_piste_rec_done_list_view);
+
+        listPisteElements.setAdapter(adapter);
+
+        listPisteElements.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Piste m = ((Piste) parent.getItemAtPosition(position));
+                        replaceSharedData(SD_PISTE_ACTUELLE, m); //
+                        //startActivity(new Intent(MainActivity.this, OpenMorceau.class));
+                    }
+                }
+        );
+
+        ((Button) findViewById(R.id.OpenMorceau_btn_new_piste)).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(OpenMorceau.this, NewPisteConfig.class));
+                    }
+                }
+        );
+    }
+
+    private ArrayAdapter<ListItemPiste> adapter ;
+    private Morceau morceau;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        List<Piste> listePiste = morceau.getListPiste();
+        adapter.clear();
+        for (Piste piste: listePiste
+                ) {
+            adapter.add(new ListItemPiste(piste));
+        }
+    }
+
+
+
+    private class ListItemPiste
+    {
+        Piste piste;
+        public ListItemPiste(Piste m)
+        {
+            this.piste=m;
+        }
+
+        public String toString()
+        {
+            return "Piste " + piste.getTypePiste().name() + " (id = "+piste.getId()+")";
+        }
+
+        public Piste getPiste()
+        {
+            return piste;
+        }
     }
 
 }
