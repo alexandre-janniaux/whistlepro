aad20b7bce1e2aa73397d3acd997b30eb2aed9ee
Mohamed <noname.p@outlook.com>
2016-05-31 10:17:09 +0200

    Ajout de l'activité de R&D pour le synthèse

---
+++ WhistlePro/app/src/main/java/fr/enst/pact34/whistlepro/app/RDSyntheseActivity.java
@@ -0,0 +1,18 @@
+package fr.enst.pact34.whistlepro.app;
+
+import android.media.AudioManager;
+import android.os.Bundle;
+
+/**
+ * Created by mms on 31/05/16.
+ */
+public class RDSyntheseActivity extends WhistleProActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rd_activity);
+    }
+
+}
